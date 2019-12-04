5821c5fb45b7b1ce6c5de677083c5d423228d3db
vu thanh hieu <vuthanhieu2000@gmail.com>
2019-12-04 20:49:05 +0700

    complete login and responsive menu

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const btnMenuClose = document.getElementById('btn-menu-close');
 const collapse = document.getElementById('collapse');
 const bgChild = document.getElementById('bg-child');
 const body = document.body;
+const login = document.getElementById('login');
+const btnLogin = document.getElementById('name-btn-login');
 
 //click show menu item
 let asNavItiemChild;
@@ -21,13 +23,18 @@ navbarNav.addEventListener('click', event => {
         asNavItiemChild = navItiemChild;
         bgChild.classList.toggle('show');
         body.classList.toggle('hidden')
-        
     }
 })
+
+//click bg child hearder in menu
 bgChild.addEventListener('click', event => {
-    asNavItiemChild.classList.remove('show');
+    if (asNavItiemChild) {
+        asNavItiemChild.classList.remove('show');   
+    }
+    login.classList.remove('show');
     bgChild.classList.remove('show');
 })
+
 //click show menu
 btnMenu.addEventListener('click', event => {
     logo.classList.add('show');
@@ -44,4 +51,10 @@ btnMenuClose.addEventListener('click', event => {
     btnMenuClose.classList.remove('show');
     collapse.classList.remove('show');
     btnMenu.classList.remove('unshow');
+})
+
+//btn login
+btnLogin.addEventListener('click', event => {
+    login.classList.toggle('show');
+    bgChild.classList.toggle('show');
 })
